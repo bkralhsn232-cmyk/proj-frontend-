81b4f0b4eaad9081b8579a0d937dfd407cda316b
Baker <bkralhsn232@gmail.com>
2026-05-20 20:51:17 +0300

    e

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -3,17 +3,15 @@ import { Navigate, Outlet } from 'react-router-dom';
 import API from '../api/axios';
 
 export default function ProtectedRoute() {
-  const [isAuthenticated, setIsAuthenticated] = useState(null);
 
-  useEffect(() => {
-    API.get('/api/auth/me')
-      .then(() => setIsAuthenticated(true))
-      .catch(() => setIsAuthenticated(false));
-  }, []);
+
+  const isAuthenticated = localStorage.getItem('isLoggedIn') === 'true';
+
+  return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
+}
 
   if (isAuthenticated === null) {
     return <h2 style={{ textAlign: 'center', marginTop: '50px' }}>🎬 Checking authorization...</h2>;
   }
 
   return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
-}
