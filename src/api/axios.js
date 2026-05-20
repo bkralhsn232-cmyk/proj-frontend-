2245c51de9c2650ab62fc6f0fd35cc6c1852d87d
Baker <bkralhsn232@gmail.com>
2026-05-20 21:58:51 +0300

    e

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,17 +1,26 @@
 import axios from 'axios';
 
-
 const API = axios.create({
-  baseURL: import.meta.env.PROD 
-    ? 'https://proj-vpn5.onrender.com' 
-    : 'http://localhost:5000',
-  withCredentials: true // ⚡ MUST BE TRUE
+  baseURL: 'https://proj-vpn5.onrender.com',
+  withCredentials: true
 });
 
+API.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export const deleteMovie = async (movieId) => {
-const response = await API.delete(`/movies/${movieId}`);
-return response.data;
+  const response = await API.delete(`/movies/${movieId}`);
+  return response.data;
 };
 
 export default API;
