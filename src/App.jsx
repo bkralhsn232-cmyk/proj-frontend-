d7389ae9495dcb1fac3cd354da275082cd082592
Baker <bkralhsn232@gmail.com>
2026-05-21 15:17:11 +0300

    e

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar';
 import Home from './pages/Home';
+import About from './pages/About'; 
 import Login from './pages/Login';
 import Register from './pages/Register';
 import AddMovie from './components/addMovie'; 
 import Forum from './pages/forum';
-import About from './pages/about';
+
 function App() {
   return (
     <div id="app-container">
@@ -13,11 +14,11 @@ function App() {
       <main className="main-content">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/add-movie" element={<AddMovie />} /> 
           <Route path="/forum/:movieId" element={<Forum />} />
-          <Route path="/about" element={<About />} />
         </Routes>
       </main>
     </div>
